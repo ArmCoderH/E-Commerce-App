037c134e7d18e3354b9eae37f03d735c1df2ed3a
Arman Hingora <armanhingora555@gmail.com>
2025-02-16 23:19:59 +0530

    setup

--- kart/src/modules/cart/index.tsx
+++ kart/src/modules/cart/index.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const App = () => {
+const Cart = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>Cart</Text>
     </View>
   )
 }
 
-export default App
+export default Cart
 
 const styles = StyleSheet.create({})
---
+++ kart/src/store/config.tsx
@@ -0,0 +1,8 @@
+import {Platform} from 'react-native'
+
+// export const BASE_URL = Platform.OS === 'android' ?
+// 'http://10.0.2.2:3000' <- for os
+// 'http://localhost:3000'<-for android 
+
+// for pysical device 
+export const BASE_URL = 'http://192.168.0.52:3000'
